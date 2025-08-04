fae6815ecbc0f68ab5c16a3bef4d4f75b6c77c7f
yeahChibyke <chibykepride@gmail.com>
2025-08-05 00:25:48 +0100

    added mocks for aggregatorv3 and btc token

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/Mockv3Aggregator.sol";
+import {MockBTC} from "../test/mocks/MockBTC.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address btcUsdPriceFeed;
+        address wBtc;
+        uint256 deployerKey;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant BTC_USD_MOCK_PRICE = 100_000e8;
+    // uint256 public constant INITIAL_BALANCE = 1_000e8;
+    uint256 public constant ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    constructor() {
+        if (block.chainid == 1_115_511) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            btcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            wBtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("ANVIL_PRIVATE_KEY")
+        });
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory anvilNetworkConfig) {
+        if (activeNetworkConfig.btcUsdPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_MOCK_PRICE);
+        MockBTC mBTC = new MockBTC(8);
+        vm.stopBroadcast();
+
+        anvilNetworkConfig = NetworkConfig({
+            btcUsdPriceFeed: address(btcUsdPriceFeed),
+            wBtc: address(mBTC),
+            deployerKey: ANVIL_PRIVATE_KEY
+        });
+    }
+}
---
+++ test/mocks/MockBTC.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockBTC is ERC20 {
+    uint8 private decimal;
+
+    constructor(uint8 _decimal) ERC20("Mock BTC", "mBTC") {
+        decimal = _decimal;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimal;
+    }
+
+    function mint(address account, uint256 amount) public {
+        uint256 mint_amount = (amount * (1 * (10 ** decimal)));
+        _mint(account, mint_amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        uint256 burn_amount = (amount * (1 * (10 ** decimal)));
+        _burn(account, burn_amount);
+    }
+
+    function transferInternal(address from, address to, uint256 value) public {
+        uint256 transfer_value = (value * (1 * (10 ** decimal)));
+        _transfer(from, to, transfer_value);
+    }
+
+    function approveInternal(address owner, address spender, uint256 value) public {
+        uint256 approve_value = (value * (1 * (10 ** decimal)));
+        _approve(owner, spender, approve_value);
+    }
+}
